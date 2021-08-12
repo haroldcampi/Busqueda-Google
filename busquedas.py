3537fda5d0ac5abe5a8e2101217b3289bc255740
Harold <hcampi92@gmail.com>
2021-08-12 14:51:35 -0300

    change name results

--- busquedas.py
+++ busquedas.py
@@ -2,7 +2,7 @@ from googlesearch import search
 
 
 termino = input("Busqueda: ") 
-cant_resultados = int(input("Cantidad de resultados: "))
+cant_results = int(input("Cantidad de resultados: "))
 
-for i in search(termino, cant_resultados, lang="es"):
+for i in search(termino, cant_results, lang="es"):
     print(i)
